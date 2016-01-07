6fc52b0dd7b6901e47325da03594c4d5b54e5a51
zumba <zumba@iki.fi>
2016-01-07 14:40:10 +0200

    [FIX] Body has message

--- src/Server/EventFactory.php
+++ src/Server/EventFactory.php
@@ -5,6 +5,7 @@ use GuzzleHttp\Client;
 use GuzzleHttp\Psr7\Request;
 use Psr\Log\LoggerAwareInterface;
 use Zumba\Beeper\Server\Event\BeeperEvent;
+use Zumba\Beeper\Server\Event\DialogNegativeAnswerEvent;
 use Zumba\Beeper\Server\Event\DialogPositiveAnswerEvent;
 use Zumba\Log\LoggingI;
 use Zumba\Log\LoggingTrait;
@@ -25,12 +26,19 @@ class EventFactory implements LoggerAwareInterface
 			throw new \InvalidArgumentException("No body in data");
 		}
 
+		if( !array_key_exists('message', $data['body']) )
+		{
+			throw new \InvalidArgumentException("No message in data body");
+		}
+
+		$message = $data['body']['message'];
+
 		switch($data['event'])
 		{
 			case BeeperEvent::DIALOG_POSITIVE_ANSWER:
-				return new DialogPositiveAnswerEvent($data['body']);
+				return new DialogPositiveAnswerEvent($message);
 			case BeeperEvent::DIALOG_NEGATIVE_ANSWER:
-				return new DialogPositiveAnswerEvent($data['body']);
+				return new DialogNegativeAnswerEvent($message);
 			default:
 				$this->logWarn("Unsupported event {event}", ['event' => $data['event']]);
 				throw new \InvalidArgumentException("Unsupported event {$data['event']}");
